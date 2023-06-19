96405ddfab90c257782659c11cdba4d3ce0a24af
eshapatel2005 <esha.patel5605@gmail.com>
2023-06-19 17:24:04 +0530

    function

---
+++ object.js
@@ -0,0 +1,10 @@
+
+let a={
+    firstName:"Esha",
+    lastName:"Patel",
+    city:"Surat",
+    fullName:function(){
+        return this.firstName + " " + this.lastName;
+    }
+};
+console.log(a.fullName());
