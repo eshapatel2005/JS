4937e20fea344448a325f4a24ab7ec52b6b20244
eshapatel2005 <esha.patel5605@gmail.com>
2023-06-23 13:08:41 +0530

    Password Valication Check

--- form.js
+++ form.js
@@ -1,9 +1,18 @@
 let form = document.querySelector('.sign-up-Form');
-// let email = document.querySelector('#email');
-// let password = document.querySelector('#password');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+let passwordPattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}";
 
 form.addEventListener('submit', e=>{
     e.preventDefault();
-    console.log(form.userEmail.value, form.userPassword.value);
+    let passwordValue = password.value;
+    console.log(passwordValue);
+    let result = passwordValue.match(passwordPattern);
+    console.log(result);
+    if (result) {
+        console.log("Your password is Strong")
+    }else {
+        console.log("Try again")
+    }
 
 })
