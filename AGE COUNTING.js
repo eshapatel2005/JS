fc2316bd8168dae48bcf70d816cb3d5161d7d6a5
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-01 17:36:36 +0530

    Object Modify

--- AGE COUNTING.js
+++ AGE COUNTING.js
@@ -5,3 +5,44 @@ let x=function(birthyear,currentyear){
 }
 console.log(x(2005,2023))
 
+
+
+<tr>
+<th>
+    <p id="fname"></p>
+</th>
+<th>
+    <p id="ftime"></p>
+</th>
+<th>
+    <p id="fcity"></p>
+</th>
+<th>
+    <p id="fnumber"></p>
+</th>
+<th>
+    <p id="fperson"></p>
+</th>
+<th>
+    <p id="femail"></p>
+</th>
+<th>
+    <p id="ftextarea"></p>
+</th>
+</tr>
+
+
+
+
+let form = document.querySelector('.sign-up-Form');
+
+ form.addEventListener('submit', e=>{
+    e.preventDefault();
+    document.getElementById("fname").innerHTML=form.username.value;
+    document.getElementById("ftime").innerHTML=form.usertime.value;
+    document.getElementById("fcity").innerHTML=form.usercity.value ;
+    document.getElementById("fnumber").innerHTML=form.usernumber.value;
+    document.getElementById("fperson").innerHTML=form.userperson.value;
+    document.getElementById("femail").innerHTML=form.useremail.value;
+    document.getElementById("ftextarea").innerHTML=form.textarea.value;
+ })
