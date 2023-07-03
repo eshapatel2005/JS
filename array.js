f9925e9e19c238c9b22921b1708b3e73aa234599
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-03 18:24:12 +0530

    sum to array in single iteration

--- array.js
+++ array.js
@@ -1,15 +1,7 @@
-//matched two array sum using for loop
 
-let a=[1,3,4,5,2];
-let b=7;
-  for(i=1;i<a.length;i++){
-    const num1 = a[i];
-    for(j=i+1;j<a.length;j++){
-      const num2 = a[j];
-      if(num1+num2===b){
-          console.log(num1);
-          console.log(num2)
-         console.log("Matched");
-      }
-    }
-  }
+//sum to array in single iteration
+
+var array1 = [1,2,3,4];
+var array2 = [5,6,7,8];
+
+var sum    = [6,8,10,12];
