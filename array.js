436976c5f6ac6a2546eb190aab9459d6449351c3
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-03 18:21:23 +0530

    matched two array sum using for loop

--- array.js
+++ array.js
@@ -1,4 +1,15 @@
-let fruits=["apple","orange","grapes"]
-for(let i=0;i<fruits.length;i++){
-    console.log(fruits[i]);
-}
+//matched two array sum using for loop
+
+let a=[1,3,4,5,2];
+let b=7;
+  for(i=1;i<a.length;i++){
+    const num1 = a[i];
+    for(j=i+1;j<a.length;j++){
+      const num2 = a[j];
+      if(num1+num2===b){
+          console.log(num1);
+          console.log(num2)
+         console.log("Matched");
+      }
+    }
+  }
