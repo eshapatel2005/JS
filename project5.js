c391596bfe906cf562b89aa425d6d95866f9c5d3
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-05 12:22:49 +0530

    "this"  keyword

--- project5.js
+++ project5.js
@@ -1,17 +1,13 @@
 
-//binary search on a sorted array
+
+//"this" keyword
 
 
-
-function binarySearch(arr,value,startPos,endPos){
-    if(startPos > endPos) return -1;
-    let middleIndex = Math.floor(startPos+endPos)/2;
-    if(arr[middleIndex] === value) return middleIndex;
-    elsif(arr[middleIndex > value]){
-    return binarySearch(arr,value,startPos,middleIndex-1);
-    }
-    else{
-     return binarySearch(arr,value,middleIndex+1,endPos);
+var obj = {
+    name: "Esha",
+    getName: function(){
+    console.log(this.name);
     }
     }
+    obj.getName();
     
