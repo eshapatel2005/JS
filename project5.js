887b2e3cabd2bd55a57c4d21cf3bb737628d1266
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-06 17:13:12 +0530

    Number pyramid pattern

--- project5.js
+++ project5.js
@@ -1,14 +1,19 @@
 
 
-//Reverse triangle
+//Number pyramid pattern
 
 
-let n = 5; // height of pattern
+let n = 5;
 let string = "";
 // External loop
 for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n - i + 1; j++) {
-    string += j;
+  // creating spaces
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
+  // creating numbers
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    string += k;
   }
   string += "\n";
 }
