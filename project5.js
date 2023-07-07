d661946d1cf0df107f3c2a39ecda76714b517d51
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-07 15:31:14 +0530

    Hourglass Pattern

--- project5.js
+++ project5.js
@@ -1,26 +1,26 @@
 
 
-//Number Diamond Pattern
+//Hourglass Pattern
 
 
 let n = 5;
 let string = "";
-// Pyramid
+// Reverse Pyramid
 for (let i = 1; i <= n; i++) {
-  for (let j = 1; j < n - i + 1; j++) {
+  for (let j = 1; j < i; j++) {
     string += " ";
   }
-  for (let k = 1; k <= 2 * i - 1; k++) {
+  for (let k = 1; k <= 2 * (n - i + 1) - 1; k++) {
     string += k;
   }
   string += "\n";
 }
-// Reverse Pyramid
+// Pyramid
 for (let i = 1; i <= n - 1; i++) {
-  for (let j = 1; j < i + 1; j++) {
+  for (let j = 1; j < n - i; j++) {
     string += " ";
   }
-  for (let k = 1; k <= 2 * (n - i) - 1; k++) {
+  for (let k = 1; k <= 2 * (i + 1) - 1; k++) {
     string += k;
   }
   string += "\n";
