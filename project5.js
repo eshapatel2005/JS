faae6a07d7bb5dbb0cc7f0ea34c3424c20763ea3
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-07 15:28:38 +0530

    Number Diamond Pattern

--- project5.js
+++ project5.js
@@ -1,18 +1,26 @@
 
 
-//Number pyramid pattern
+//Number Diamond Pattern
 
 
 let n = 5;
 let string = "";
-// External loop
+// Pyramid
 for (let i = 1; i <= n; i++) {
-  // creating spaces
-  for (let j = 1; j < i; j++) {
+  for (let j = 1; j < n - i + 1; j++) {
     string += " ";
   }
-  // creating numbers
-  for (let k = 1; k <= 2 * (n - i + 1) - 1; k++) {
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    string += k;
+  }
+  string += "\n";
+}
+// Reverse Pyramid
+for (let i = 1; i <= n - 1; i++) {
+  for (let j = 1; j < i + 1; j++) {
+    string += " ";
+  }
+  for (let k = 1; k <= 2 * (n - i) - 1; k++) {
     string += k;
   }
   string += "\n";
