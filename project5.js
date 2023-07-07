91a5edb89a637d2e1c1dafad0d3f0ff274c7a539
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-07 15:27:29 +0530

    Reverse Pyramid Pattern

--- project5.js
+++ project5.js
@@ -8,11 +8,11 @@ let string = "";
 // External loop
 for (let i = 1; i <= n; i++) {
   // creating spaces
-  for (let j = 1; j <= n - i; j++) {
+  for (let j = 1; j < i; j++) {
     string += " ";
   }
   // creating numbers
-  for (let k = 1; k <= 2 * i - 1; k++) {
+  for (let k = 1; k <= 2 * (n - i + 1) - 1; k++) {
     string += k;
   }
   string += "\n";
