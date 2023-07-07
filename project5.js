57cb78a5b1a6ac4ccf550d7b69357092bdbbe7bc
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-07 15:32:47 +0530

    Number Pascal Pattern

--- project5.js
+++ project5.js
@@ -1,26 +1,20 @@
 
 
-//Hourglass Pattern
+//Number Pascal Pattern
 
 
 let n = 5;
 let string = "";
-// Reverse Pyramid
+// Pyramid
 for (let i = 1; i <= n; i++) {
-  for (let j = 1; j < i; j++) {
-    string += " ";
-  }
-  for (let k = 1; k <= 2 * (n - i + 1) - 1; k++) {
+  for (let k = 1; k <= i; k++) {
     string += k;
   }
   string += "\n";
 }
-// Pyramid
+// Reverse Pyramid
 for (let i = 1; i <= n - 1; i++) {
-  for (let j = 1; j < n - i; j++) {
-    string += " ";
-  }
-  for (let k = 1; k <= 2 * (i + 1) - 1; k++) {
+  for (let k = 1; k <= n - i; k++) {
     string += k;
   }
   string += "\n";
