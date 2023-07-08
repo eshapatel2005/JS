c025e79bea5a7a53ed0f73b82d67fc6b967fe127
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-08 15:51:34 +0530

    Right Triangle Pattern in Javascript

--- project5.js
+++ project5.js
@@ -1,28 +1,19 @@
 
 
-//Hollow Square Pattern
+//Right Triangle Pattern in Javascript
 
 
-let n = 5; // row or column count
-// defining an empty string
+let n = 5;
 let string = "";
-
-for(let i = 0; i < n; i++) { // external loop
-  for(let j = 0; j < n; j++) { // internal loop
-    if(i === 0 || i === n - 1) {
-      string += "*";
-    }
-    else {
-      if(j === 0 || j === n - 1) {
-        string += "*";
-      }
-      else {
-        string += " ";
-      }
-    }
+for (let i = 1; i <= n; i++) {
+  // printing spaces
+  for (let j = 0; j < n - i; j++) {
+    string += " ";
   }
-  // newline after each row
+  // printing star
+  for (let k = 0; k < i; k++) {
+    string += "*";
+  }
   string += "\n";
 }
-// printing the string
 console.log(string);
