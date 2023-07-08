e5ce5096f3efbcd92ae6fe4d040c05f4a2aaf668
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-08 15:52:28 +0530

    Left Triangle Pattern in Javascript

--- project5.js
+++ project5.js
@@ -1,17 +1,12 @@
 
 
-//Right Triangle Pattern in Javascript
+//Left Triangle Pattern in Javascript
 
 
 let n = 5;
 let string = "";
 for (let i = 1; i <= n; i++) {
-  // printing spaces
-  for (let j = 0; j < n - i; j++) {
-    string += " ";
-  }
-  // printing star
-  for (let k = 0; k < i; k++) {
+  for (let j = 0; j < i; j++) {
     string += "*";
   }
   string += "\n";
