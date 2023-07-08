a418a2c6930622d78a0cf58da50a55e689750cb0
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-08 15:50:23 +0530

    Hollow Square Pattern

--- project5.js
+++ project5.js
@@ -1,6 +1,6 @@
 
 
-//Square Star Pattern
+//Hollow Square Pattern
 
 
 let n = 5; // row or column count
@@ -9,7 +9,17 @@ let string = "";
 
 for(let i = 0; i < n; i++) { // external loop
   for(let j = 0; j < n; j++) { // internal loop
-    string += "*";
+    if(i === 0 || i === n - 1) {
+      string += "*";
+    }
+    else {
+      if(j === 0 || j === n - 1) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
   }
   // newline after each row
   string += "\n";
