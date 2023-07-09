248761e5a4ed17d97bcb07c30738a895a89e1dea
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-09 08:45:14 +0530

    Pyramid  Star Pattern

--- project5.js
+++ project5.js
@@ -1,25 +1,19 @@
 
 
-//Hollow Triangle Star Pattern
+// Pyramid  starPattern
 
 
-let n = 6;
+let n = 5;
 let string = "";
-
+// External loop
 for (let i = 1; i <= n; i++) {
+  // printing spaces
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
   // printing star
-  for (let j = 0; j < i; j++) {
-    if(i === n) {
-      string += "*";
-    }
-    else {
-      if (j == 0 || j == i - 1) {
-        string += "*";
-      }
-      else {
-        string += " ";
-      }
-    }
+  for (let k = 0; k < 2 * i - 1; k++) {
+    string += "*";
   }
   string += "\n";
 }
