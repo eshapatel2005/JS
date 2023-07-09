cc060ae9be726e94d82c8c3d06f8ccfcaab90a41
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-09 08:43:53 +0530

    Hollow Triangle Star Pattern

--- project5.js
+++ project5.js
@@ -1,14 +1,25 @@
 
 
-//Downward Triangle Star Pattern
+//Hollow Triangle Star Pattern
 
 
-let n = 5;
+let n = 6;
 let string = "";
-for (let i = 0; i < n; i++) {
+
+for (let i = 1; i <= n; i++) {
   // printing star
-  for (let k = 0; k < n - i; k++) {
-    string += "*";
+  for (let j = 0; j < i; j++) {
+    if(i === n) {
+      string += "*";
+    }
+    else {
+      if (j == 0 || j == i - 1) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
   }
   string += "\n";
 }
