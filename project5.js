e9f8a1104a294f43ba007fc4f3f8a1987d2f1957
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-10 18:39:02 +0530

    Hollow Pyramid Star Pattern

--- project5.js
+++ project5.js
@@ -1,19 +1,30 @@
 
 
-// Reversed Pyramid Star Pattern
+// Hollow Pyramid Star Pattern
 
 
 let n = 5;
 let string = "";
+
 // External loop
-for (let i = 0; i < n; i++) {
+for (let i = 1; i <= n; i++) {
   // printing spaces
-  for (let j = 0; j < i; j++) {
+  for (let j = 1; j <= n - i; j++) {
     string += " ";
   }
   // printing star
-  for (let k = 0; k < 2 * (n-i) - 1; k++) {
-    string += "*";
+  for (let k = 0; k < 2 * i - 1; k++) {
+    if(i === 1 || i === n) {
+      string += "*";
+    }
+    else {
+      if(k === 0 || k === 2 * i - 2) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
   }
   string += "\n";
 }
