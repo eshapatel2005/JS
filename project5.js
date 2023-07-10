83e0af0e59171cc6d5a422f88432d8755be7b5db
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-10 18:42:24 +0530

    Hollow star Diamond Pattern

--- project5.js
+++ project5.js
@@ -1,11 +1,11 @@
 
 
-// Diamond  star Pattern in Javascript
-
+//Hollow star Diamond Pattern
 
 let n = 5;
 let string = "";
 // Upside pyramid
+// upside diamond
 for (let i = 1; i <= n; i++) {
   // printing spaces
   for (let j = n; j > i; j--) {
@@ -13,20 +13,30 @@ for (let i = 1; i <= n; i++) {
   }
   // printing star
   for (let k = 0; k < i * 2 - 1; k++) {
-    string += "*";
+    if (k === 0 || k === 2 * i - 2) {
+      string += "*";
+    }
+    else {
+      string += " ";
+    }
   }
   string += "\n";
 }
-// downside pyramid
+// downside diamond
 for (let i = 1; i <= n - 1; i++) {
-  // printing spaces
-  for (let j = 0; j < i; j++) {
-    string += " ";
+    // printing spaces
+    for (let j = 0; j < i; j++) {
+      string += " ";
+    }
+    // printing star
+    for (let k = (n - i) * 2 - 1; k >= 1; k--) {
+      if (k === 1 || k === (n - i) * 2 - 1) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
+    string += "\n";
   }
-  // printing star
-  for (let k = (n - i) * 2 - 1; k > 0; k--) {
-    string += "*";
-  }
-  string += "\n";
-}
 console.log(string);
