55091667e2bc0539d908f9b2b0d978dacdb8ac09
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-11 17:30:49 +0530

    Right Pascal Star Pattern

--- project5.js
+++ project5.js
@@ -1,29 +1,19 @@
 
 
-//Hourglass Star Pattern
+//Right Pascal Star Pattern
+
+
 
 let n = 5;
 let string = "";
-// Reversed pyramid pattern
-for (let i = 0; i < n; i++) {
-  // printing spaces
+for (let i = 1; i <= n; i++) {
   for (let j = 0; j < i; j++) {
-    string += " ";
-  }
-  // printing star
-  for (let k = 0; k < (n - i) * 2 - 1; k++) {
     string += "*";
   }
   string += "\n";
 }
-// pyramid pattern
-for (let i = 2; i <= n; i++) {
-  // printing spaces
-  for (let j = n; j > i; j--) {
-    string += " ";
-  }
-  // printing star
-  for (let k = 0; k < i * 2 - 1; k++) {
+for (let i = 1; i <= n - 1; i++) {
+  for (let j = 0; j < n - i; j++) {
     string += "*";
   }
   string += "\n";
