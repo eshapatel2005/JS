94197dab0a1a85530be328c84ba6921be366e24e
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-11 17:31:35 +0530

    Left Pascal Star Pattern

--- project5.js
+++ project5.js
@@ -1,19 +1,25 @@
 
 
-//Right Pascal Star Pattern
+// Left Pascal Star Pattern
 
 
 
 let n = 5;
 let string = "";
 for (let i = 1; i <= n; i++) {
-  for (let j = 0; j < i; j++) {
+  for (let j = 0; j < n - i; j++) {
+    string += " ";
+  }
+  for (let k = 0; k < i; k++) {
     string += "*";
   }
   string += "\n";
 }
 for (let i = 1; i <= n - 1; i++) {
-  for (let j = 0; j < n - i; j++) {
+  for (let j = 0; j < i; j++) {
+    string += " ";
+  }
+  for (let k = 0; k < n - i; k++) {
     string += "*";
   }
   string += "\n";
